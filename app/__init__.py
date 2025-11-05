37d94a0ed42f25798da17f2eaadb078725c14754
muzzzic546-ui <muzzzic.546@gmail.com>
2025-11-05 11:31:58 -0500

    Testing (#4)
    * refactor the winner determination logic
    
    * Testing

---
+++ app/__init__.py
@@ -0,0 +1,7 @@
+# this init file can be blank 
+
+# and it helps python and pytest understand
+
+# the app direcotry has some code in it
+
+# that can be imported
---
+++ conftest.py
@@ -0,0 +1,7 @@
+# this conftest.py file 
+
+# helps pytest
+
+# understand the structure of the read me
+
+# and find the app code
